75a0fd3313218185d8aba0aa28002271d3c75a3e
Charis Koutsis <xariskoutsis@gmail.com>
2025-05-04 01:46:33 +0300

    Updated account information by setting currency rates

--- utils/account_functions.py
+++ utils/account_functions.py
@@ -1,7 +1,13 @@
+from typing import Dict, List, Optional
+
 from tradernet import TraderNetAPI
 
+import utils as get_cross_rates
 
-def get_api_access(public_key, private_key, login, password):
+
+def get_api_access(
+    public_key: str, private_key: str, login: str, password: str
+) -> TraderNetAPI:
     """
     Creates and returns an instance of TraderNetAPI to access the API.
 
@@ -14,13 +20,10 @@ def get_api_access(public_key, private_key, login, password):
     Returns:
     - TraderNetAPI: An instance of TraderNetAPI initialized with the provided credentials.
     """
-    return TraderNetAPI(public_key, 
-                        private_key, 
-                        login, 
-                        password)
+    return TraderNetAPI(public_key, private_key, login, password)
 
 
-def get_d_account_user_info(api_access):
+def get_d_account_user_info(api_access: TraderNetAPI) -> List[Dict]:
     """
     Retrieves the EUR and USD Account balances for a user via TraderNetAPI.
 
@@ -28,40 +31,74 @@ def get_d_account_user_info(api_access):
     - api_access (TraderNetAPI): An instance of TraderNetAPI for accessing user account information.
 
     Returns:
-    - list: A list containing a dictionary with 'EUR' and 'USD' keys, representing the respective balances.
+    - List: A list containing a dictionary with 'EUR' and 'USD' keys, representing the respective balances.
     """
-    eur_amount = TraderNetAPI.account_summary(api_access)['result']['ps']['acc'][0]['s']
-    usd_amount = TraderNetAPI.account_summary(api_access)['result']['ps']['acc'][1]['s']
+    # Get the account summary
+    # The account summary contains the account balances in different currencies
+    eur_amount = TraderNetAPI.account_summary(api_access)["result"]["ps"]["acc"][0]["s"]
+    usd_amount = TraderNetAPI.account_summary(api_access)["result"]["ps"]["acc"][1]["s"]
 
-    data = {'EUR': eur_amount,
-            'USD': usd_amount
-    }
+    data = {"EUR": eur_amount, "USD": usd_amount}
     return [data]
 
 
-def get_trading_account_user_info(api_access):
+def get_trading_account_user_info(
+    api_access: TraderNetAPI, currency: Optional[str] = "EUR"
+) -> List[Dict]:
     """
     Retrieves Trading Account information for a user via TraderNetAPI.
 
     Args:
     - api_access (TraderNetAPI): An instance of TraderNetAPI for accessing user account information.
+    - currency Optional[str]: The currency in which to retrieve the account information. Default is "EUR".
 
     Returns:
-    - list: A list containing dictionaries with trading account details.
+    - List: A list containing dictionaries with trading account details.
     """
     all_data = []
-    positions = TraderNetAPI.account_summary(api_access)['result']['ps']['pos']
 
-    for i in range(len(positions)):
+    # Get the account summary
+    positions = TraderNetAPI.account_summary(api_access)["result"]["ps"]["pos"]
+
+    # Get the EUR to USD rate
+    eurusd_currecy_rate = get_cross_rates.get_cross_rates(
+        base_currency="EUR", currencies=["USD"]
+    )
+    eurusd_currecy_rate_value = eurusd_currecy_rate["USD"]
+
+    # Compute USD to EUR as reciprocal
+    usdeur_currecy_rate_value = 1 / eurusd_currecy_rate_value
+
+    # Iterate through the positions and extract relevant information
+    for position in positions:
+
+        # Check if the position is in the specified currency
+        base_currency = position["base_currency"]
+
+        # Get the position details
+        qty = position["q"]
+        price = position["mkt_price"]
+        entry_price = round(position["price_a"], 2)
+        profit = position["profit_close"]
+        conversion_rate = 1
+
+        # Determine the conversion rate based on the base currency and the specified currency
+        if base_currency == "EUR" and currency == "USD":
+            conversion_rate = eurusd_currecy_rate_value
+        elif base_currency == "USD" and currency == "EUR":
+            conversion_rate = usdeur_currecy_rate_value
+
+        # Calculate the profit in the specified currency
         data = {
-            'Ticker': positions[i]['base_contract_code'],
-            'Qty': positions[i]['q'],
-            'Entry Price': float(f"{positions[i]['price_a']:.2f}"),
-            'Price': positions[i]['mkt_price'],
-            'Value': float(f"{positions[i]['mkt_price']*positions[i]['q']:.2f}"),
-            'Profit': positions[i]['profit_close']
+            "Ticker": position["base_contract_code"],
+            "Qty": qty,
+            "Entry Price": entry_price,
+            "Price": price,
+            "Value": round(price * qty * conversion_rate, 2),
+            "Profit": round(profit * conversion_rate, 2),
         }
+
+        # Append the data to the list
         all_data.append(data)
+
     return all_data
-
-
